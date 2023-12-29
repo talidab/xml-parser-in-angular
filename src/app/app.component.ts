678244bb2a13f82eed3c5cfe75a9729165b77cbb
talidab <taly01232@yahoo.com>
2023-12-29 13:13:21 +0200

    styled the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,21 @@ import * as xml2js from 'xml2js';
 export class AppComponent {
 
   xmlString: string = '';
+  jsonData: any;
+
+  buttonLabel: string = 'Apasa aici pentru a vedea ce animale se afla la gradina zoologica';
+  getAnimalsClicked: boolean = false;
 
   constructor(private appService: AppService) {}
 
   ngOnInit(): void {
     const xmlUrl = 'assets/baza-de-cunostinte.xml';
-    this.appService.getXmlData(xmlUrl).subscribe((data) => {
+    this.appService.getXmlData(xmlUrl).subscribe((data: any) => {
       this.xmlString = data;
-      console.log(this.xmlString); // Output the XML string
-
-      // Parse XML string to JSON
+      // console.log(this.xmlString); 
       this.parseXmlString();
     });
+    
   }
 
   private parseXmlString(): void {
@@ -29,9 +32,22 @@ export class AppComponent {
       if (error) {
         console.error('Error parsing XML:', error);
       } else {
-        const jsonData = data;
-        console.log(jsonData); // Output the parsed JSON data
+        this.jsonData = data;
+        console.log(this.jsonData);
       }
     });
   }
+
+  animals: any[] = [];
+
+  getAnimals(): void {
+    this.getAnimalsClicked = true;
+    this.animals = this.jsonData.gradina_zoologica.animale.animal;
+    console.log(this.animals);
+    for (const animal of this.animals){
+      console.log(animal.nume);
+    }
+  }
+
+  displayMessage: string = '';
 }
